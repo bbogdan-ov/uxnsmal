fc3e0aa716264163c60673c09e2eca0ba89650ed
bogdanov <bbogdan_ov@mail.ru>
2025-08-25 23:15:22 +0300

    move tests into `tests/`

---
+++ uxnsmal/tests/lexer.rs
@@ -0,0 +1,97 @@
+use uxnsmal::lexer::{Keyword as Kw, Lexer, Radix, TokenKind::*};
+
+#[test]
+fn lexer_all_tokens() {
+	const S: &str = r#"
+		const hey var a
+		data b
+
+		$0o77
+
+		fun my-vec ( -> ) {}
+		fun my-func ( -- ) {
+			10 20*hello
+
+			loop @label {
+				jump@label jumpif @label
+				0b101 0xff add pop
+			}
+			@block {}
+
+			"my-string!!"
+			"esc\"apes\" 'char' \\"
+			'a'
+			'ab\n'
+			'\\'
+		}
+	"#;
+
+	#[rustfmt::skip]
+	let expect = [
+		("const", Keyword(Kw::Const)),
+		("hey", Ident),
+		("var", Keyword(Kw::Var)),
+		("a", Ident),
+		("data", Keyword(Kw::Data)),
+		("b", Ident),
+
+		("$", Dollar),
+		("0o77", Number(Radix::Octal)),
+
+		("fun", Keyword(Kw::Func)),
+		("my-vec", Ident),
+		("(", OpenParen),
+		("->", ArrowRight),
+		(")", CloseParen),
+		("{", OpenBrace),
+		("}", CloseBrace),
+
+		("fun", Keyword(Kw::Func)),
+		("my-func", Ident),
+		("(", OpenParen),
+		("--", DoubleDash),
+		(")", CloseParen),
+		("{", OpenBrace),
+			("10", Number(Radix::Decimal)),
+			("20", Number(Radix::Decimal)),
+			("*", Asterisk),
+			("hello", Ident),
+
+			("loop", Keyword(Kw::Loop)),
+			("@", AtSign),
+			("label", Ident),
+			("{", OpenBrace),
+				("jump", Keyword(Kw::Jump)),
+				("@", AtSign),
+				("label", Ident),
+				("jumpif", Keyword(Kw::JumpIf)),
+				("@", AtSign),
+				("label", Ident),
+				("0b101", Number(Radix::Binary)),
+				("0xff", Number(Radix::Hexadecimal)),
+				("add", Ident),
+				("pop", Ident),
+			("}", CloseBrace),
+			("@", AtSign),
+			("block", Ident),
+			("{", OpenBrace),
+			("}", CloseBrace),
+
+			(r#""my-string!!""#, String),
+			(r#""esc\"apes\" 'char' \\""#, String),
+			(r#"'a'"#, Char),
+			(r#"'ab\n'"#, Char),
+			(r#"'\\'"#, Char),
+		("}", CloseBrace),
+		("", Eof),
+	];
+
+	let tokens = Lexer::parse(S).unwrap();
+	assert_eq!(tokens.len(), expect.len());
+	for idx in 0..expect.len() {
+		let tok = &tokens[idx];
+		let slice = &S[tok.span.into_range()];
+
+		assert_eq!((slice, tok.kind), expect[idx]);
+	}
+}
